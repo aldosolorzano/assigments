9bab491b930a51fd2a9aa9e01a87138287f5240f
delano <delano@solutious.com>
2017-01-29 18:54:28 -0800

    Remove logging line and update whitespacing

--- jukebox/jukebox.js
+++ jukebox/jukebox.js
@@ -3,39 +3,37 @@ let keys = ["pitch", "beats"];
 let notes = prompt("Pleas write the notes you want to play and the beat of each one");
 let bpm = prompt("Please write the bpm");
 
-
-const splitAx = function(str, wich){ //removes * from notes
+const splitAx = function(str, wich){  //removes * from notes
   let arr = str.split("*");
   return arr[wich];
 }
 
-const splitLet = function(str){ //r
+const splitLet = function(str){       //r
   let arr = str.split("*");
- return arr[0];
+  return arr[0];
 }
-
 
 let parseNote = function(note){
   var music = [];
   let arr = note.split(" ");
 
-  for(var i =0, len = arr.length; i <len; i++) {
+  for(var i =0, len = arr.length; i < len; i++) {
     let obj = new Object();
 
     for(var j =0, l=keys.length; j<l; j++) {
       if(arr[i].length > 2 && j > 0) {
-        obj[keys[j]] = parseInt(splitAx(arr[i],1));
+        obj[keys[j]] = parseInt(splitAx(arr[i], 1));
       } else if(j > 0) {
         obj[keys[j]] = 1;
       } else {
-         obj[keys[j]] = splitAx(arr[i],0);
+         obj[keys[j]] = splitAx(arr[i], 0);
       }
     }
 
     music.push(obj);
   }
-console.log(music);
-return music;
+
+  return music;
 }
 
 
