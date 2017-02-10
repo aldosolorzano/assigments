87627f5882d42495437e059cff35d43279b23269
aldosolorzano <aldosolorzanop@gmail.com>
2017-02-09 18:30:40 -0800

    CRUD working

--- pm_tool/app/controllers/projects_controller.rb
+++ pm_tool/app/controllers/projects_controller.rb
@@ -1,4 +1,47 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
+
+  def show
+    @project = Project.find params[:id]
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    project_params = params.require(:project).permit([:title,:description])
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def destroy
+    @project = Project.find params[:id]
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  def update
+    project_params = params.require(:project).permit([:title,:description])
+    @project = Project.find(params[:id])
+
+    if Project.update(project_params)
+      redirect_to project_path(@project)
+    else
+      render :edit
+    end
+
+  end
+
 end
--- pm_tool/config/routes.rb
+++ pm_tool/config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
   get '/' => 'home#index'
   get '/about' => 'home#about'
 
+  get '/projects' => 'projects#index'
+  get 'projects/new' => 'projects#new', as: :new_project
+  get '/projects/:id' => 'projects#show', as: :project
+  get '/project/edit/:id' => 'projects#edit', as: :edit_project
+  post '/projects' => 'projects#create'
+  delete '/projects/:id' => 'projects#destroy'
+  patch '/projects/:id' => 'projects#update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
