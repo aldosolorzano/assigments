ccfc2b9c2c7d547af8baf871c4e95bea73ab2c2a
delano <delano@solutious.com>
2017-02-14 20:26:11 -0800

    Grouped test code together, added logging, and fix for call to unshift

--- advance-JavaScript/stack&queue.js
+++ advance-JavaScript/stack&queue.js
@@ -3,22 +3,12 @@ const Stack =  function(stacker){
   this.stacker = [];
 }
 Stack.prototype.add = function (arg){
-  this.stacker.unshift(arg);
+  this.stacker.push(arg);
 }
 Stack.prototype.remove = function(){
   return this.stacker.pop(); // remove the last element
 }
 
-a = new Stack();
-a.add("plate1");
-a.add("plate2");
-a.add("plate3");
-a.add("plate4");
-a.add("plate5");
-a.add("plate6");
-
-a.remove()
-a.remove()
 
 // A queue is First-In First-Out (FIFO) system.
 class Queue {
@@ -35,6 +25,19 @@ class Queue {
   }
 }
 
+a = new Stack();
+a.add("plate1");
+a.add("plate2");
+a.add("plate3");
+a.add("plate4");
+a.add("plate5");
+a.add("plate6");
+
+console.log(1, a.stacker);
+a.remove();
+a.remove();
+console.log(2, a.stacker);
+
 b = new Queue();
 b.add("glass1");
 b.add("glass2");
@@ -43,5 +46,9 @@ b.add("glass4");
 b.add("glass5");
 b.add("glass6");
 
+console.log(3, b.queue);
 b.remove()
 b.remove()
+console.log(4, b.queue);
+
+
